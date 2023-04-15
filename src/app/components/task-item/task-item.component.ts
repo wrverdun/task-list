27a7e4193cba28515d0c573a514e2b2a8aac78db
wrverdun <wrverdun@hotmail.com>
2023-04-15 18:34:24 -0300

    se agrego style a task-item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from '../mock_tasks';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
+
+  @Input() task:Task=TASKS[0]
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
